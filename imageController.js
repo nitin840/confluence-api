16578f55fe857e413ccbc915637d2333c178ae4e
nitink840 <59434214+nitink840@users.noreply.github.com>
2020-02-05 18:21:22 +0530

    added confluence api

--- imageController.js
+++ imageController.js
@@ -1,56 +1,17 @@
 var express = require('express')
 var router = express.Router()
-var multer = require('multer')
-var path = require('path')
 var Image = require('./imageModel')
 var bodyParser = require('body-parser')
 var jwt = require('jsonwebtoken')
+var request = require('request')
 
 //image storage
-var storage = multer.diskStorage({
-    destination: './uploads/images',
-    filename: (req,file,cb)=>{
-        cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
-    }
-})
 
-var upload = multer({
-    storage : storage
-})
 
 router.use(bodyParser.urlencoded({extended:true}))
 router.use(bodyParser.json())
-router.use('/image',express.static('upload/images'))
 
-// insert data
-router.post('/insert',upload.single('image'),(req,res)=>{
 
-    if(!req.body.email || !req.body.name || !req.file){
-        return  res.status(400).json({
-            message : "Full data required"
-        })
-    }
-    //create a image
-    const image = new Image({
-        name: req.body.name,
-        email: req.body.email,
-        image:req.file.destination+"/"+req.file.filename
-    })
-    //save the image
-    image.save()
-    .then(data => res.status(200).json({
-            message : "Success",
-            response:{
-                image_url : "http://localhost:8080/image/"+req.file.filename,
-                data: data
-            }
-    }))
-    .catch(err=>{
-        res.status(500).json({
-            message:"unable to create record in database "+err.message
-        })
-    })
-})
 const verifyToken = (req,res,next) => {
     var tokenHeader = req.get('Authorization')
     if(typeof tokenHeader !== 'undefined'){
@@ -61,157 +22,6 @@ const verifyToken = (req,res,next) => {
         res.status(403).send({message : "split token error "+ req.token})
     }
 }
-
-//get all images
-router.get('/getAll',verifyToken,(req,res)=>{
-
-    jwt.verify(req.token,'secretKey',(err, payLoad)=>{
-        if(err){
-            res.status(403).send({
-                message : "token verify error "+ err.message
-            })
-        }
-
-        Image.find({})
-        .then(data=>{
-            if(!data){
-                return res.status(400).json({
-                    message:"No records found!"
-                })
-            }
-        res.status(200).json({payLoad, data})
-        })
-        .catch(err => {
-            res.status(500).json({
-                message:"Unable to retrive data "+err.message
-            })
-        })
-
-    })
-})
-
-//get one by email
-router.get('/get/:email',(req,res)=>{
-    if(!req.params.email){
-        return res.status(400).json({
-            message : "Unique is required"
-        })
-    }
-
-    Image.findOne(req.params)
-    .then(data =>{
-        if(!data){
-            return res.status(404).json({
-                message:"Cannot find data with id "+ req.params.email
-            })
-        }
-        res.status(200).json({
-            message: "Success",
-            data: data,
-        })
-    })
-    .catch(err=>{
-        if(err.kind === 'ObjectId') {
-            return res.status(404).send({
-                message: "Record not found with id " + req.params.email
-            });                
-        }
-        res.status(500).send({
-            message : "Unable to fetch data "+err.message
-        })
-    })
-})
-
-//update record 
-router.put('/update/:email',(req,res)=>{
-    let email =req.params.email
-    if(!req.params.email || !req.body){
-        return res.status(400).json({
-            message : "Bad request "
-        })
-    }
-    Image.findOneAndUpdate(req.params,req.body,{new:true})
-    .then(data =>{
-        if(!data){
-            return res.status(404).json({
-                message: "No record found with email "+ email
-            })
-        }
-        res.status(200).json({
-            message : "Updated succesfully",
-            data : data
-        })
-    })
-    .catch(err =>{
-        if(err.kind === 'ObjectId') {
-            return res.status(404).send({
-                message: "Record not found with email " + email
-            });                
-        }
-        res.status(500).json({
-            message : "Unable to update data "+err.message
-        })
-    })
-})
-
-//delete record
-router.delete('/delete/:email',(req,res)=>{
-    
-    let email =req.params.email
-    if(!req.params.email){
-        return res.status(400).json({
-            message: "Bad request"
-        })
-    }
-    Image.findOneAndDelete(req.params)
-    .then(data =>{
-        if(!data){
-            return res.status(404).json({
-                message:"No record found with email "+email
-            })
-        }
-        res.status(200).json({
-            message: "successfully record deleted with email "+email
-        })
-    })
-    .catch(err => {
-        if(err.kind === 'ObjectId' || err.name === 'NotFound'){
-            res.status(404).json({
-                message : "No record found with id "+email +", "+ err.message
-            })
-        }
-        res.status(500).json({
-            message : "Unable to delete record " + err.message
-        })
-    })
-})
-
-router.post('/register',upload.single('image'),(req,res) => {
-    let body = req.body
-    let file = req.file
-    if(Object.keys(req.body).length === 0 || !req.file){
-        return res.status(400).json({
-            message : "All data required",
-        })
-    }
-    const image = new Image({
-        email: body.email,
-        name : body.name,
-        image : file.destination+"/"+file.filename
-    })
-
-    image.save()
-    .then(data => {
-            res.status(200).json({
-                message : "Successfully registered with " + data.name
-            })
-    })
-    .catch(err =>{
-        res.status(500).json({
-            message : "Unable to register "+ err.message
-        })
-    })
-})
 
 router.post('/login',(req,res) => {
 
@@ -228,7 +38,7 @@ router.post('/login',(req,res) => {
                 message : "No user found with this credentials"
             })
         }
-        jwt.sign({data},'secretKey',{expiresIn:'30s'},(err, token)=>{
+        jwt.sign({data},'secretKey',(err, token)=>{
             if(err){
                 return res.send({message : "token error "+err.message })
             }
@@ -237,6 +47,226 @@ router.post('/login',(req,res) => {
     })
     .catch(err => res.status(500).json({ message : "Unable to find user "+ err.message}))
 
+})
+
+//jeets api
+router.get('/fetch',(req,res)=>{
+    axios.get('https://confluence-cab62.firebaseio.com/data.json')
+    .then(data =>{
+        if(!data){
+            return res.status(404).json({
+                message:"Not found!"
+            })
+        }
+        res.status(200).json({
+            message: "success",
+            data:data.data,
+        })
+    })
+    .catch(err=>{
+        res.json({
+            message: "Unable to fetch data "+err,
+        })
+        console.log(err.message)
+    })
+})
+
+//getting page content
+router.get('/atlassian',(req,res)=>{
+
+     var options = {
+        method: 'GET',
+        url: 'https://yapihew.atlassian.net/wiki/rest/api/content',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Basic eWFwaWhldzY3NUBtYWlsZmlsZS5vcmc6ZDA5Y0hIeEhCMVdlbWM2RzVLemVBNUUw'
+        }
+    }
+    request(options,function(error,response,body){
+
+        if (error) {
+            throw new Error(error);
+             res.json({
+                 message :"erroor : ",error
+             })
+        }
+        console.log(
+         'Response: ' + response.statusCode + ' ' + response.statusMessage
+      );
+        console.log(body);
+        res.json({
+         message: "successfully",
+         data: JSON.parse(body)
+        });
+     
+ })
+    
+    })
+
+//getting page template
+router.get('/ok',(req,res)=>{
+
+    var options = {
+       method: 'GET',
+       url: 'https://yapihew.atlassian.net/wiki/rest/api/template/7733501',
+       headers: {
+           'Content-Type': 'application/json',
+           'Authorization': 'Basic eWFwaWhldzY3NUBtYWlsZmlsZS5vcmc6ZDA5Y0hIeEhCMVdlbWM2RzVLemVBNUUw'
+       }
+   }
+
+    request(options,function(error,response,body){
+
+           if (error) {
+               throw new Error(error);
+                res.json({
+                    message :"erroor : ",error
+                })
+           }
+           console.log(
+            'Response: ' + response.statusCode + ' ' + response.statusMessage
+         );
+         var a =JSON.parse(body)
+           //console.log(a," O K ");
+           res.json(JSON.stringify(a.body.storage.value));
+           console.log(JSON.stringify(a.body.storage.value))
+        
+    })
+   
+})
+
+//simply creating page from bodyData
+router.post('/confluence/:title/:content',(req,res)=>{
+
+    const titlevalue = req.params.title;
+    const bodyvalue = req.params.content;
+
+    var bodyData = `{
+        "id":"8093825",
+        "title":"${titlevalue}",
+        "type":"page",
+        "space":{"key":"PROPAGE"},
+        "status":"current","ancestors":[],
+        "body":{
+            "storage":{
+                "_expandable":{
+                    "content":"/rest/api/content/8093825"
+                },
+                "representation":"storage",
+                "value": "${bodyvalue}"
+            }
+        }
+    }`;
+   
+    
+    //res.send(bodyData)
+    var options = {
+        method: 'POST',
+        url: 'https://yapihew.atlassian.net/wiki/rest/api/content',
+        headers: {
+           'Content-Type': 'application/json',
+           'Authorization': 'Basic eWFwaWhldzY3NUBtYWlsZmlsZS5vcmc6ZDA5Y0hIeEhCMVdlbWM2RzVLemVBNUUw'
+        },
+        body: bodyData
+     };
+     
+     request(options, function (error, response, body) {
+        //if (error) throw new Error(error);
+        if (error) {
+           // throw new Error(error);
+            res.json({
+                 message :"erroor : ",error
+             });
+        }
+        console.log(
+           'Response: ' + response.statusCode + ' ' + response.statusMessage
+        );
+        console.log(body);
+        res.send(body)
+     });
+
+})
+
+//get template and create page
+router.post('/createPage/:title',(req,res)=>{
+
+    //getting template with id
+    var options = {
+        method: 'GET',
+        url: 'https://yapihew.atlassian.net/wiki/rest/api/template/7733501',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Basic eWFwaWhldzY3NUBtYWlsZmlsZS5vcmc6ZDA5Y0hIeEhCMVdlbWM2RzVLemVBNUUw'
+        }
+    }
+ 
+     request(options,function(error,response,body){
+ 
+            if (error) {
+               // throw new Error(error);
+                 res.json({
+                     message :"erroor : ",error
+                 })
+            }
+           
+           // console.log(body);
+
+            const templateData = JSON.parse(body); //to access inside object
+    var temp = JSON.stringify(templateData.body.storage.value);
+
+
+            //template body;
+          //  console.log("tempData : ",templateData," tmapdlfjbshfbhjddddd")
+            var bodyData = `{
+                "id":"8093825",
+                "title":"${req.params.title}",
+                "type":"page",
+                "space":{"key":"PROPAGE"},
+                "status":"current","ancestors":[],
+                "body":{
+                    "storage":{
+                        "_expandable":{
+                            "content":"/rest/api/content/8093825"
+                        },
+                        "representation":"storage",
+                        "value": ${temp}
+                    }
+                }
+            }`;
+            function myFunction(str) {
+                var n = str.search("JP");
+                return n;
+                //document.getElementById("demo").innerHTML = n;
+              }
+          // console.log(templateData, " raavan")
+            res.json(myFunction(temp))
+           
+            //create page with template body
+            // var options1 = {
+            //     method: 'POST',
+            //     url: 'https://yapihew.atlassian.net/wiki/rest/api/content',
+            //     headers: {
+            //        'Content-Type': 'application/json',
+            //        'Authorization': 'Basic eWFwaWhldzY3NUBtYWlsZmlsZS5vcmc6ZDA5Y0hIeEhCMVdlbWM2RzVLemVBNUUw'
+            //     },
+            //     body: bodyData
+            //  };
+             
+            //  request(options1, function (error, response, body) {
+            //     //if (error) throw new Error(error);
+            //     if (error) {
+            //        // throw new Error(error);
+            //         res.json({
+            //              message :"erroor : ",error
+            //          });
+            //     }
+            //     console.log(
+            //        'Response: ' + response.statusCode + ' ' + response.statusMessage
+            //     );
+            //     console.log(body);
+            //     res.send(body)
+            //  });
+     });   
 })
 
 module.exports = router
