f19db83385c5f946b4fefbe36f7b43908710a433
Nitin Kushwaha <nitin@metapercept.com>
2020-03-11 16:53:06 +0530

    my commit message

--- index1.js
+++ index1.js
@@ -1,1 +1,1 @@
-my new file contents
+const express = require('express') const app = express() var db = require('./db') var imageRouter = require('./imageController') app.use('/image',imageRouter) module.exports = app
