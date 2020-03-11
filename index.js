5e99ab13f3d0acd54d61cc6157598fd5103401df
Nitin Kushwaha <nitin@metapercept.com>
2020-03-11 15:33:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-const express = require('express') const app = express() var db = require('./db') var imageRouter = require('./imageController') app.use('/image',imageRouter) module.exports = app
+const express = require('express') 
+const app = express() var db = require('./db') 
+var imageRouter = require('./imageController') 
+app.use('/image',imageRouter) 
+module.exports = app
